6f27d7eabf8c655a127e72a61b4827bc04a60d4e
Helio Barbosa <barbosahifb1997@gmail.com>
2026-04-30 12:35:39 -0100

    Consume API

--- assets/icons/badge-check.tsx
+++ assets/icons/badge-check.tsx
@@ -6,9 +6,9 @@ export const BadgeCheckIcon = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="#13A4EC"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="lucide lucide-badge-check-icon lucide-badge-check">
             <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
             <path d="m9 12 2 2 4-4" />
--- components/login/right-section.tsx
+++ components/login/right-section.tsx
@@ -1,10 +1,55 @@
+"use client"
+
 import Link from "next/link"
 import { Button } from "../ui/button"
 import { Card, CardContent, CardHeader } from "../ui/card"
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
+import { useState } from "react"
 
 export const RightSection = () => {
+    //useState
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const changeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value) {
+            setEmail(e.target.value)
+        } else {
+            setEmail("")
+        }
+    };
+
+    const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value) {
+            setPassword(e.target.value)
+        } else {
+            setPassword("")
+        }
+    };
+    const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        //fetch API
+        await fetch(
+            'http://localhost:8080/users/login',
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "aplication/json"
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            }
+        ).then((response) => {
+            console.log(response.json())
+        });
+    };
+
+    console.log({email: email, password: password});
+
+
     return (
         <div className="w-1/2 flex flex-col justify-center">
             <Card className="h-full flex flex-col justify-center px-14 gap-16">
@@ -16,21 +61,27 @@ export const RightSection = () => {
                     <div className="flex flex-col gap-5">
                         <div className="flex flex-col gap-2">
                             <Label>Email</Label>
-                            <Input type="text" placeholder="example@example.com" className="py-4 text-lg h-10" />
+                            <Input type="text" placeholder="example@example.com" className="py-4 text-lg h-10"
+                                value={email}
+                                onChange={changeEmail} />
                         </div>
 
                         <div className="flex flex-col gap-2">
                             <Label>Password</Label>
-                            <Input type="password" placeholder="your password..." className="py-4 text-lg h-10" />
+                            <Input type="password" placeholder="your password..." className="py-4 text-lg h-10"
+                                value={password}
+                                onChange={changePassword} />
                         </div>
 
-                        <Button className="bg-[#13A4EC] rounded-md text-white font-bold py-3 drop-shadow-lg drop-shadow-gray-200">Login</Button>
+                        <Button
+                        onClick={handleLogin}
+                        className="bg-[#13A4EC] rounded-md text-white font-bold py-3 drop-shadow-lg drop-shadow-gray-200">Login</Button>
                     </div>
 
-                        <div>
-                            <span>Don't have an account cont?</span>
-                            <Link href="//registo" className="text-[#13A4EC] font-semibold">Create Account</Link>
-                        </div>
+                    <div>
+                        <span>Don't have an account cont?</span>
+                        <Link href="//registo" className="text-[#13A4EC] font-semibold">Create Account</Link>
+                    </div>
                 </CardContent>
             </Card>
         </div>
